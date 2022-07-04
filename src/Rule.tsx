0b000de502cb423e99e2c10ab32fe568a939c151
corentinleberre <corentin.leb29@gmail.com>
2022-07-04 09:58:20 +0200

    :recycle: Refacto du module de règle dans un composant dédié

--- src/Rule.tsx
+++ src/Rule.tsx
@@ -1,5 +1,57 @@
-function Rule() {
-    return (<p>Vous devez créer une nouvelle règle</p>);
+import { useState } from "react";
+import { Action } from "./model";
+
+function Rule(props: { addRule: (action: Action) => void}) {
+
+    const [dice1, setDice1] = useState<number>(0);
+    const [dice2, setDice2] = useState<number>(0);
+    const [label, setLabel] = useState<string>('');
+    const [drink, setDrink] = useState<boolean>(false);
+
+    const isFormValid = (): boolean => dice1 > 0 && dice2 > 0 && label.length > 0;
+
+    return (
+        <>
+            <p>Vous devez créer une nouvelle règle</p>
+            <div>
+              <div>
+                <label htmlFor="dice1">Dès n°1</label>
+                <input
+                  type="number"
+                  name='dice1'
+                  value={dice1}
+                  onChange={e => setDice1(e.target.valueAsNumber)}
+                />
+                <label htmlFor="dice2">Dès n°2</label>
+                <input
+                  type="number"
+                  name='dice2'
+                  value={dice2}
+                  onChange={e => setDice2(e.target.valueAsNumber)}
+                />
+              </div>
+              <div>
+                <label htmlFor="description">Description</label>
+                <input
+                  type='text'
+                  name='description'
+                  value={label}
+                  onChange={e => setLabel(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="drink">Drink</label>
+                <input
+                  type="checkbox"
+                  name='drink'
+                  defaultChecked={drink}
+                  onChange={_ => setDrink(!drink)}
+                />
+              </div>
+              <button type="button" disabled={!isFormValid()} onClick={() => props.addRule({dices: [dice1, dice2], label: label, drink: drink})}>Ajouter la règle</button>
+            </div>
+        </>
+    );
 }
 
 export default Rule;
